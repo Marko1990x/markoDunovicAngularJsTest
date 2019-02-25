538b3577df8ebc1a050d9ab090bf16adf8affb41
Marko Dunovic <marko_dunovic@hotmail.com>
2019-02-25 16:45:16 +0100

    init
    this is the initial commit

---
+++ phone/phone.controller.js
@@ -0,0 +1,21 @@
+angular.module('MainApp').controller('PhoneController', function($scope){
+    $scope.title = 'Phone page';
+    $scope.phones = [
+        {
+            name: "Nexus 5",
+            snippet: "Ovo je deo 1X",
+            id: 1
+        },
+        {
+            name: "Bla bla 3",
+            snippet: "Ovo je deo 2",
+            id: 2
+        },
+        {
+            name: "Nokia",
+            snippet: "Ovo je deo 3",
+            id: 3
+        }
+        
+    ]
+})
